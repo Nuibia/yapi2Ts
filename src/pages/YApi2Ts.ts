ce900dfa5f5addc55130c971a996d95b2fa453fe
jiangniao <jiangniao@juwan.org>
2023-05-08 17:06:39 +0800

    feat: 支持一键复制请求和响应参数

--- src/pages/YApi2Ts.ts
+++ src/pages/YApi2Ts.ts
@@ -1,4 +1,117 @@
-export const YApi2Ts = (data:any) => {
-    console.log(data);
-    
+interface IParams {
+  /** 备注 */
+  desc: string;
+  /** 例子  */
+  example: string;
+  /** 字段名 */
+  name: string;
+  /** 是否必选 */
+  required: string;
+  /** 类型 */
+  type: string;
+  _id: string;
 }
+interface IResult {
+  type: string;
+  title: string;
+  description: string;
+}
+
+export const YApi2Ts = (data: any) => {
+  console.log(data);
+  const {
+    /** 接口名称 */
+    title,
+    /** 接口创建人 */
+    username,
+    /** 接口路径 */
+    path,
+    /** body请求参数 */
+    req_body_form,
+    /** 返回数据-响应报文 */
+    res_body,
+    /** query请求参数 */
+    req_query,
+  } = data;
+
+  return {
+    // 请求参数
+    queryParams: ReturnParams(req_query, path),
+    bodyParams: ReturnParams(req_body_form, path),
+    resultData:ReturnResult(res_body, path)
+  };
+
+  // 详情参数
+  // 请求主体
+};
+
+// 请求params
+export const ReturnParams = (data: IParams[], path: string) => {
+  let params = `export interface I${finallyCode(path)}Params {`;
+  data.forEach((item) => {
+    params += `/** ${item.desc} */`;
+    params += `${item.name}`;
+    params += `${item.required === "0" ? "?" : ""}:`;
+    params += `${TypeObj?.[item.type] ?? "any"}`;
+    params += `;`;
+  });
+  params += `}`;
+  if(!data?.length) return '';
+  return params;
+};
+// 响应result
+export const ReturnResult = (data: string, path: string) => {
+  const JsonData: { [s: string]: IResult } =
+    JSON.parse(data).properties.data.properties;
+  console.log(JsonData);
+
+  let params = `export interface I${finallyCode(path)}Result {`;
+  params += formatResultBase(JsonData);
+  params += `}`;
+  return params;
+};
+
+/** 后端-前端类型映射 */
+export const TypeObj: Record<string, string> = {
+  text: "string",
+  string:"string",
+  long: "number",
+  number: "number",
+  boolean: "boolean",
+};
+
+/** 获取路径的最后一个/后的值，并首字母大写 */
+export const finallyCode = (path: string) => {
+  const array = path.split("/");
+  return (
+    array?.[array.length - 1][0].toUpperCase() +
+    array[array.length - 1].slice(1)
+  );
+};
+
+export const formatObject = (data: { [s: string]: IResult }) => {
+  let finallyCode = "";
+  for (const [key, value] of Object.entries(data)) {
+    finallyCode += `/** ${value.description} */`;
+    finallyCode += `${key}:`;
+    finallyCode += `${TypeObj?.[value.type]};`;
+  }
+  return finallyCode;
+};
+
+export const formatResultBase = (data: { [s: string]: any }) => {
+  let finallyCode = "";
+  for (const [key, value] of Object.entries(data)) {
+    if (value.type === "object") {
+      finallyCode += `${key}:{`;
+      finallyCode += formatObject(value.properties);
+      finallyCode += `};`;
+    }
+    if (value.type === "array") {
+      finallyCode += `${key}:{`;
+      finallyCode += `${formatObject(value.items.properties)}`;
+      finallyCode += `}[]`;
+    }
+  }
+  return finallyCode;
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,82 @@
-import styles from './index.less';
-import {Button, Form, Input, Select} from 'antd';
-import axios from 'axios';
+import styles from "./index.less";
+import { Button, Form, Input, Select, Space, message } from "antd";
+import axios from "axios";
 import { YApi2Ts } from "@/pages/YApi2Ts";
+import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
 const index = () => {
-  
-  const onFinish = async(values:{token:string,interfaceId:string}) => {
-    const {token,interfaceId} = values;    
-    
-    if(token && interfaceId){
-      const res = await axios(`/api/interface/get?token=${token}&id=${interfaceId}`);
-      if(res?.data?.data) {
-        YApi2Ts(res.data.data);
+  const [params, setParams] = useState<string>();
+  const [result, setResult] = useState<string>();
+
+  const onFinish = async (values: { token: string; interfaceId: string }) => {
+    const { token, interfaceId } = values;
+
+    if (token && interfaceId) {
+      const res = await axios(
+        `/api/interface/get?token=${token}&id=${interfaceId}`
+      );
+      if (res?.data?.data) {
+        const { queryParams, bodyParams, resultData } = YApi2Ts(res.data.data);
+        setParams(queryParams ?? bodyParams);
+        setResult(resultData);
       }
       // 获取请求form-body类型的请求参数
     }
-  }
-  return <div className={styles.Container}>
-    <h1 className={styles.contentTitle}>YApi2TS</h1>
-    <div className={styles.contentFilter}>
-      <Form layout="inline" onFinish={onFinish}>
-        <Form.Item label="选择项目组" name="token" required>
-        <Select
-            style={{ width: 120 }}
-        options={[
-          {value:'59101d71c85ee7f6d2bda25e888c1ef174d1be97994de106ae4b83f3aff3d5b8',label:'CP'},
-        ]}/>                  
-        </Form.Item>
-        <Form.Item label="接口id" name="interfaceId" required>
-          <Input placeholder="请输入接口id" allowClear/>
-        </Form.Item>
-        <Form.Item>
-        <Button htmlType="submit" type="primary">提交</Button>
-        </Form.Item>
-      </Form>
+  };
+  return (
+    <div className={styles.Container}>
+      <h1 className={styles.contentTitle}>YApi2TS</h1>
+      <div className={styles.contentFilter}>
+        <Form layout="inline" onFinish={onFinish}>
+          <Form.Item label="选择项目组" name="token" required>
+            <Select
+              style={{ width: 120 }}
+              options={[
+                {
+                  value:
+                    "59101d71c85ee7f6d2bda25e888c1ef174d1be97994de106ae4b83f3aff3d5b8",
+                  label: "CP",
+                },
+              ]}
+            />
+          </Form.Item>
+          <Form.Item label="接口id" name="interfaceId" required>
+            <Input placeholder="请输入接口id" allowClear />
+          </Form.Item>
+          <Form.Item>
+            <Button htmlType="submit" type="primary">
+              提交
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+      <div className={styles.codeContent}>
+        <Space>
+          {params && (
+            <CopyToClipboard
+              text={params ?? ""}
+              onCopy={() => {
+                message.success("复制成功");
+              }}
+            >
+              <Button>复制请求参数</Button>
+            </CopyToClipboard>
+          )}
+          {result && (
+            <CopyToClipboard
+              text={result ?? ""}
+              onCopy={() => {
+                message.success("复制成功");
+              }}
+            >
+              <Button>复制响应参数</Button>
+            </CopyToClipboard>
+          )}
+        </Space>
+      </div>
     </div>
-    <div className={styles.codeContent}>
-    </div>
-  </div>;
+  );
 };
 
 export default index;
